920e4b2706374ea2bd68ef888f70dae1a49b0748
Ahmed Hussin <ahmed208087@gmail.com>
2023-09-24 16:56:11 +0300

    check and uncheck functionality added delete task functionality added, data are saved in the local storage now

--- Todo-app/app.js
+++ Todo-app/app.js
@@ -8,6 +8,30 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li)
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
     }
     inputBox.value=""
+    saveData()
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")//if clicked on the li it will add checked class if it's already added it will remove it because we added toggle
+        saveData()
+    }
+
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()//if we clicked on the span it will remove the parent element which is the li 
+        saveData()
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask()
